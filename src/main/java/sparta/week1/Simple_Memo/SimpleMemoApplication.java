e3ba608cecbec853d731ab494ab7c8fd8e965c4e
ckwogus37 <ckwogus37@gmail.com>
2025-01-18 16:26:05 +0900

    최초 커밋

---
+++ src/main/java/sparta/week1/Simple_Memo/SimpleMemoApplication.java
@@ -0,0 +1,13 @@
+package sparta.week1.Simple_Memo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleMemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleMemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sparta/week1/Simple_Memo/SimpleMemoApplicationTests.java
@@ -0,0 +1,13 @@
+package sparta.week1.Simple_Memo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleMemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
